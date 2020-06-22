2461f15728ab2fbae89ed34ed5fd59138d192304
GarlandW <61223105+GarlandW@users.noreply.github.com>
2020-06-22 22:41:01 +0100

    Add files via upload

---
+++ contracts/Election.sol
@@ -0,0 +1,71 @@
+pragma solidity 0.4.24;
+
+contract Election {
+    
+	struct Voter {
+        bool voted;
+        int voteconf;
+    }
+	struct OptionCh {
+        int counter;
+        string opchoice;
+        int voteCount;
+    }
+	
+		event UserVote (
+        int indexed OptionPKey
+    );
+
+	 int public OpCt; 
+
+    mapping(address => Voter) public voters;
+
+    mapping(int => OptionCh) public optionmap;
+
+	function OptionConstruction (string foo) private {
+    OpCt += 1;
+	optionmap[OpCt] = OptionCh(OpCt, foo, 0);
+	}
+
+	function authorization(address voter) private {
+    voters[voter].voteconf = 1;
+    }
+
+
+	/*
+	address public creator;
+	creator = 0x0C282453C9943A14767338f9c2C9608dB343FbD4;
+	int public creatorveri;
+
+
+
+	function creatorperm () public {
+		require(msg.sender == creator);
+		creatorveri = 1;
+	}
+
+
+	*/
+    
+	constructor () public {
+	    OptionConstruction("Option 1");
+        OptionConstruction("Option 2");
+	    OptionConstruction("Option 3");
+		OptionConstruction("Option 4");
+		authorization(0x09d8BB02Bd4Ce750fB4B421acF0A818778A38C74);
+		// creatorperm(); 
+    }
+
+
+    function vote (int OptionPKey) public {
+        
+		require(voters[msg.sender].voted == false);
+		require(voters[msg.sender].voteconf == 1);
+
+        voters[msg.sender].voted = true;
+
+        optionmap[OptionPKey].voteCount += voters[msg.sender].voteconf;
+
+        emit UserVote(OptionPKey);
+    }
+}
---
+++ src/js/app.js
@@ -0,0 +1,116 @@
+App = {
+    web3Provider: null,
+    contracts: {},
+    VoteCF: false,
+
+    init: function () {
+        return App.Connection();
+    },
+
+    Connection: function () {
+        if (typeof web3 !== 'undefined') {
+            App.web3Provider = web3.currentProvider;
+            ethereum.enable();
+            web3 = new Web3(web3.currentProvider);
+        } else {
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+            web3 = new Web3(App.web3Provider);
+        }
+        return App.initialize();
+    },
+
+    initialize: function () {
+        $.getJSON("Election.json", function (Election) {
+            App.contracts.Election = TruffleContract(Election);
+            App.contracts.Election.setProvider(App.web3Provider);
+            App.actlist();
+            return App.uiload();
+        });
+    },
+
+    
+    actlist: function () {
+        App.contracts.Election.deployed().then(function (instance) {
+            instance.UserVote({}, {
+                fromBlock: 0,
+                toBlock: 'latest'
+            }).watch(function (error, event) {
+                console.log("event triggered", event)
+                App.uiload();
+            });
+        });
+    },
+
+    uiload: function () {
+        var electionInstance;
+        var instructions = $("#instructions");
+        var formsubmission = $("#formsubmission");
+        var processed = $("#votecounter");
+
+        instructions.show();
+        formsubmission.hide();
+        processed.hide();
+        web3.eth.getCoinbase(function (err, account) {
+            if (err === null) {
+                App.account = account;
+                $("#ethereumaccount").html("Current Ethereum Address:" + account);
+            }
+        });
+
+        App.contracts.Election.deployed().then(function (instance) {
+            electionInstance = instance;
+            return electionInstance.OpCt();
+        }).then(function (OpCt) {
+
+             //  var Optionresults = $("#Optionresults");
+            //   Optionresults.empty();
+            var optionform = $('#optionform');
+            optionform.empty();
+
+            for (var i = 1; i <= OpCt; i++) {
+                electionInstance.optionmap(i).then(function (op) {
+                    var counter = op[0];
+                    var opchoice = op[1];
+               //   var votecount = op[2];
+              //    return electionInstance.creatorveri();
+               //   if (creatorveri = 1){
+               //  var rest = "<tr><th>" + counter + "</th><td>" + opchoice + "</td><td>" + voteCount + "</td></tr>"
+               //   Optionresults.append(rest);
+               //   }
+                    var Optionarr = "<option value='" + counter + "' >" + opchoice + "</ option>"
+                    optionform.append(Optionarr);
+                });
+            }
+            return electionInstance.voters(App.account);
+        }).then(function (VoteCF) {
+            if (VoteCF) {
+                $('form').hide();
+                $("#votecounter").show();
+                $("#instructions").hide();
+            }
+            instructions.hide();
+            formsubmission.show();
+        }).catch(function (error) {
+            console.warn(error);
+        });
+    },
+
+    VoteCheck: function () {
+        var PKey = $('#optionform').val();
+        App.contracts.Election.deployed().then(function (instance) {
+            return instance.vote(PKey, { from: App.account });
+        }).then(function (result) {
+            $("#formsubmission").hide();
+            $("#instructions").show();
+            $("#votecounter").hide();
+        }).catch(function (err) {
+            console.error(err);
+        });
+    }
+};
+$(function () {
+    $(window).load(function () {
+        App.init();
+    });
+});
+
